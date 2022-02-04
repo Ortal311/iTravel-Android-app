85826a4ffbcea3c761a76ef8bbc398a6eb154a7b
ortal <ortallik@gmail.com>
2022-02-04 19:52:07 +0200

    Add/edit picture - post & user(gallery and camera) + layout changes.

--- app/src/main/java/com/example/itravel/post/PostEditFragment.java
+++ app/src/main/java/com/example/itravel/post/PostEditFragment.java
@@ -1,18 +1,27 @@
 package com.example.itravel.post;
 
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
 
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -20,6 +29,9 @@ import android.widget.TextView;
 import com.example.itravel.R;
 import com.example.itravel.model.Model;
 import com.example.itravel.model.Post;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class PostEditFragment extends Fragment {
 
@@ -31,7 +43,12 @@ public class PostEditFragment extends Fragment {
     Button cancelBtn;
     String postId;
     ProgressBar progressBar;
-
+    ImageButton cameraBtn;
+    ImageButton galleryBtn;
+    String photo = "";
+    Bitmap imageBitmap;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_GALLERY = 2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,6 +64,8 @@ public class PostEditFragment extends Fragment {
         dropdown = view.findViewById(R.id.editpost_dropdown);
         saveBtn = view.findViewById(R.id.editpost_save_btn);
         cancelBtn = view.findViewById(R.id.editpost_cancel_btn);
+        cameraBtn = view.findViewById(R.id.editpost_camera_btn);
+        galleryBtn = view.findViewById(R.id.editpost_gallery_btn);
 
         postId = PostDetailsFragmentArgs.fromBundle(getArguments()).getPostId();
 
@@ -58,6 +77,20 @@ public class PostEditFragment extends Fragment {
             }
         });
 
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,9 +99,7 @@ public class PostEditFragment extends Fragment {
                     @Override
                     public void onComplete(Post post) {
                         String difficulty = dropdown.getSelectedItem().toString();
-                        savePost(post,titleEt.getText().toString(), descriptionEt.getText().toString(), locationEt.getText().toString(),difficulty);
-                        Model.instance.refreshPostList();
-                        Navigation.findNavController(v).navigateUp();
+                        savePost(post,titleEt.getText().toString(), descriptionEt.getText().toString(), locationEt.getText().toString(),difficulty, view);
                     }
                 });
             }
@@ -82,17 +113,39 @@ public class PostEditFragment extends Fragment {
         return view;
     }
 
-    private void savePost(Post post, String title,String description,String location,String difficulty ) {
-        saveData(post, title, description , location , difficulty);
-        Model.instance.updatePost(post);
-        Model.instance.UpdatePost(post, () -> {});
+    private void savePost(Post post, String title,String description,String location,String difficulty, View v) {
+        saveData(post, title, description , location , difficulty,  v);
     }
 
-    private void saveData(Post post,String title,String description,String location,String difficulty) {
+    private void saveData(Post post,String title,String description,String location,String difficulty, View v) {
         post.setTitle(title);
         post.setLocation(location);
         post.setDescription(description);
         post.setDifficulty(difficulty);
+
+        if (imageBitmap != null) {
+            Model.instance.savePostImage(imageBitmap, post.getId() + ".jpg", new Model.SaveImageListener() {
+                @Override
+                public void onComplete(String url) {
+                    post.setPhoto(url);
+                    Log.d("TAG", "url - " + url);
+                    Model.instance.updatePost(post);
+                    Model.instance.UpdatePost(post, () -> {});
+                    Model.instance.addPhotoToPost(post, new Model.AddPhotoToPost() {
+                        @Override
+                        public void onComplete() {
+
+                        }
+                    });
+                    Model.instance.refreshPostList();
+                    Navigation.findNavController(v).navigateUp();
+                }
+            });
+        }else {
+            Model.instance.updatePost(post);
+            Model.instance.UpdatePost(post, () -> {});
+            Navigation.findNavController(v).navigateUp();
+        }
     }
 
     public void displayPost(String title, String location, String description,String difficulty)
@@ -118,5 +171,41 @@ public class PostEditFragment extends Fragment {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, itemsDropdown);
         dropdown.setAdapter(adapter);
         dropdown.setSelection(pos);
+    }
+
+
+    private void openGallery() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, REQUEST_IMAGE_GALLERY);
+    }
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                imageBitmap = (Bitmap) extras.get("data");
+            }
+        } else if (requestCode == REQUEST_IMAGE_GALLERY) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    final Uri imageUri = data.getData();
+                    final InputStream imageStream = getContext().getContentResolver().openInputStream(imageUri);
+                    imageBitmap = BitmapFactory.decodeStream(imageStream);
+                } catch (FileNotFoundException e) {
+                    Log.d("TAG", "ERRORRRR");
+                    e.printStackTrace();
+//                    Toast.makeText(PostImage.this, "Something went wrong", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        }
     }
 }
